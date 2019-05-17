d5b484fc916e9c1e400a35ebc9d097e3074e5588
Andrew McCluskey <andrew@ajmccluskey.com>
2019-05-17 14:00:26 +1000

    Take org as an argument to the exe.

--- gh-stats.nix
+++ gh-stats.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, bytestring, github, lens, stdenv, sv }:
+{ mkDerivation, base, bytestring, github, lens, stdenv, sv, text }:
 mkDerivation {
   pname = "gh-stats";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ base bytestring github lens sv ];
+  libraryHaskellDepends = [ base bytestring github lens sv text ];
   executableHaskellDepends = [ base ];
   homepage = "https://qfpl.io";
   description = "Pull Github stats for an organisation and its repos";
--- src/GhStats/Main.hs
+++ src/GhStats/Main.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
 import qualified Data.ByteString.Lazy as LBS
 import           Data.Foldable        (toList)
 import           Data.Sv              (defaultEncodeOptions, encodeNamed)
+import qualified Data.Text            as T
+import           GitHub               (mkOrganizationName)
+import           System.Environment   (getArgs)
+
 import           GhStats              (getOrgStats, repoStatsEnc)
 
 main ::
@@ -12,5 +14,6 @@ main ::
 main = do
   let
     dumpCsv = LBS.putStr . encodeNamed repoStatsEnc defaultEncodeOptions . toList
-  es <- getOrgStats "qfpl"
+  (org:_) <- getArgs
+  es <- getOrgStats . mkOrganizationName $ T.pack org
   either print dumpCsv es
